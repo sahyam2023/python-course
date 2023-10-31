40a4aea9e3018448fc1f7f7b2aec4ad5cb536d3c
sahyam2023 <sahyam@lpu.in>
2023-11-01 03:24:22 +0530

    improved gross pay

--- projects/grosspay.py
+++ projects/grosspay.py
@@ -1,3 +1,9 @@
-hours = input('Enter Hours\n')
-rate = input('Enter Rate\n')
-print(f'Your pay is {round(float(hours) * float(rate), 2)}')
+hours = int(input('Enter Hours\n'))
+rate = float(input('Enter Rate\n'))
+if hours < 40:
+    pay= round (hours *rate, 2)
+else:
+    overtime = hours-40
+    pay= round(40 * rate + overtime *rate*1.5, 2)
+
+print(f'Your pay is {pay}')
