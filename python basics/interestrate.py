6d6c605390aa005c84a7f27cf7754454ed305409
sahyam2023 <sahyam@lpu.in>
2023-11-01 01:05:41 +0530

    learning python

--- python basics/interestrate.py
+++ python basics/interestrate.py
@@ -1,12 +1,23 @@
 print("welcome to my calulator")
+Rate=0
 salary = int(input('Enter your salary \n'))
-if(salary < 2000):
-    print(f'sorry no morgage for you')
-else:
+if(salary > 2000):
     creditScore = int(input('Enter your credit score \n'))
     if(creditScore > 800):
-        print('Your are eligible for morgage and your interest rate will be 4%')
+        Rate=4
+        print('Your are eligible for morgage and your interest rate can be 4%')
     elif(creditScore >600 and creditScore<800):
-        print('Your are eligible for morgage and your interest rate will be 5%')
+        Rate=5
+        print('Your are eligible for morgage and your interest rate can be 5%')
     else:
-        print('Your are eligible for morgage and your interest rate will be 6%')
+        Rate=6
+        print('Your are eligible for morgage and your interest rate can be 6%')
+    disability = input("Do you have any disability? Type y for Yes and n for No\n")
+    if disability == 'y':
+        Rate -= 2
+        print(f'You got a discount and now your final Interest rate is{Rate}%')
+    else:
+        Rate = Rate
+        print(f'Sorry, your interest rate is still the same i.e {Rate}%.')
+else:
+    print('Sorry you are not eligible for morgage')
